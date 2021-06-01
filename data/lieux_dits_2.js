03961932b4b79946caba7e9b117af5787b89fdf9
Olotomoi <85202548+Olotomoi@users.noreply.github.com>
2021-06-02 00:50:17 +0100

    Add files via upload

---
+++ data/lieux_dits_2.js
@@ -0,0 +1,26 @@
+var json_lieux_dits_2 = {
+"type": "FeatureCollection",
+"name": "lieux_dits_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nom": "Zeke_Zeke" }, "geometry": { "type": "Point", "coordinates": [ 779851.845239530899562, 436292.723634115536697 ] } },
+{ "type": "Feature", "properties": { "Nom": "socropole_Mess" }, "geometry": { "type": "Point", "coordinates": [ 779747.49223928200081, 436190.669772689056117 ] } },
+{ "type": "Feature", "properties": { "Nom": "Belgocam_Mess" }, "geometry": { "type": "Point", "coordinates": [ 779825.542581286397763, 436277.339539593376685 ] } },
+{ "type": "Feature", "properties": { "Nom": "Las_vegas_Mess" }, "geometry": { "type": "Point", "coordinates": [ 780056.850632688612677, 435560.883694565622136 ] } },
+{ "type": "Feature", "properties": { "Nom": "Congelcam_Mess" }, "geometry": { "type": "Point", "coordinates": [ 779460.11961376783438, 436261.241070060932543 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 779011.997476593824103, 435735.182334934710525 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 779755.260025586816482, 435493.693924780294765 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 780030.358573866193183, 435098.860902499523945 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 780131.802084007882513, 435045.548109893337823 ] } },
+{ "type": "Feature", "properties": { "Nom": "Hotel Tehas sebis" }, "geometry": { "type": "Point", "coordinates": [ 779093.11792359710671, 436502.940131147915963 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 778491.607351924409159, 436699.833447017415892 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 778648.967113036545925, 436748.544529629813042 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 780354.667375194258057, 435007.543218128499575 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 779228.122476053307764, 435092.397121170361061 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 779171.491447934182361, 435064.553497350425459 ] } },
+{ "type": "Feature", "properties": { "Nom": null }, "geometry": { "type": "Point", "coordinates": [ 779779.282101071556099, 435136.655997321067844 ] } },
+{ "type": "Feature", "properties": { "Nom": "CCA" }, "geometry": { "type": "Point", "coordinates": [ 779718.896341867628507, 436144.845631257281639 ] } },
+{ "type": "Feature", "properties": { "Nom": "Melrose place Hotel" }, "geometry": { "type": "Point", "coordinates": [ 779736.283096047700383, 436152.855259587406181 ] } },
+{ "type": "Feature", "properties": { "Nom": "college Enfants d'Afrique" }, "geometry": { "type": "Point", "coordinates": [ 779466.375402191886678, 436457.825491105788387 ] } }
+]
+}
